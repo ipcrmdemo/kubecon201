ded0b51546b8531c0245fcb7ba00201e37d53105
Matt Cadorette <mcadorette@gmail.com>
2018-12-13 11:21:45 -0800

    Initial commit from Atomist

---
+++ src/main/java/com/kubecon201/Kubecon201Application.java
@@ -0,0 +1,12 @@
+package com.kubecon201;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kubecon201Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kubecon201Application.class, args);
+	}
+}
---
+++ src/test/java/com/kubecon201/Kubecon201ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kubecon201;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Kubecon201ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
